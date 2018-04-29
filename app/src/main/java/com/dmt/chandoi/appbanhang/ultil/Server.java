38948fd960425075af506be517abfab4f91ecc52
nguyenduc1997 <ducnguyenptit1909@gmail.com>
2018-04-29 18:33:53 +0700

    Push code to Github

---
+++ app/src/main/java/com/dmt/chandoi/appbanhang/ultil/Server.java
@@ -0,0 +1,15 @@
+package com.dmt.chandoi.appbanhang.ultil;
+
+/**
+ * Created by Admin on 4/14/2018.
+ */
+
+public class Server {
+
+    public static String duongdanloaisp = "http://192.168.1.107:81/server/getloaisanpham.php";
+    public static String duongdansanphammoinhat = "http://192.168.1.107:81/server/getsanphammoinhat.php";
+    public static String duongdandienthoai = "http://192.168.1.107:81/server/getsanpham.php?page=";
+    public static String duongdanlaptop = "http://192.168.1.107:81/server/getsanpham.php?page=1";
+    public static String duongdandonhang = "http://192.168.1.107:81/server/thongtinkhachhang.php";
+    public static String duongdanchitietdonhang = "http://192.168.1.107:81/server/chitietdonhang.php";
+}
